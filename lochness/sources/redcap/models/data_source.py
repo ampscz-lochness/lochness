fad817d254d2c93e6c88362eb16e15ec0063c89e
Dheshan Mohandass <dmohandass@mgh.harvard.edu>
2026-04-23 09:22:31 -0400

    fix: Redcap data dictionary overlap (#80)
    * fix: All site level REDCap metadata getting updated for each indicudual REDCap source
    
    * feat: Add data_source_name parameter to limit sync scope for REDCap data dictionary
    
    * fix: Remove unnecessary filtering of non-main REDCap data sources in refresh_redcap_dictionary function

--- lochness/sources/redcap/models/data_source.py
+++ lochness/sources/redcap/models/data_source.py
@@ -105,6 +105,7 @@ class RedcapDataSource(BaseModel):
         config_file: Path,
         project_id: str,
         site_id: str,
+        data_source_name: str,
         dictionary: List[Dict[str, Any]],
     ) -> None:
         """
@@ -130,6 +131,7 @@ class RedcapDataSource(BaseModel):
         )
         WHERE project_id = '{project_id}'
             AND site_id = '{site_id}'
+            AND data_source_name = '{data_source_name}'
             AND data_source_type = 'redcap'
         """
 
